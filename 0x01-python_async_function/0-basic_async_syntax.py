5e5ad531856a60152fdb2029f4d103833de4e95e
chingling008 <130581550+chingling008@users.noreply.github.com>
2024-10-15 08:32:17 +0000

    first_fileWorks

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+import asyncio
 import random
 
-async def wait_random(max_delay=10):
-    """
-    Waits for a random amount of time between 0 and max_delay (inclusive) seconds
-    using asyncio.sleep and returns the actual delay.
 
-    Args:
-        max_delay (int, optional): The maximum delay in seconds. Defaults to 10.
-
-    Returns:
-        float: The actual delay that the coroutine waited for.
-    """
-
-    delay = random.uniform(0, max_delay)  # Generate random delay
-    await asyncio.sleep(delay)  # Pause for the random delay
-    return delay                 # Return the actual delay
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
--- 0x01-python_async_function/main.py
+++ 0x01-python_async_function/main.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 
 import asyncio
-from importlib import import_module  # Use import_module for dynamic import
 
-async def main():
-    """
-    Calls the wait_random coroutine multiple times with different max_delay values
-    and prints the results using asyncio.run.
-    """
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-    wait_random = import_module('0-basic_async_syntax').wait_random  # Import coroutine
-
-    print(f"Result 1: {await asyncio.run(wait_random())}")
-    print(f"Result 2: {await asyncio.run(wait_random(5))}")
-    print(f"Result 3: {await asyncio.run(wait_random(15))}")
-
-if __name__ == "__main__":
-    asyncio.run(main())  # Run the main coroutine
+print(asyncio.run(wait_random()))
+print(asyncio.run(wait_random(5)))
+print(asyncio.run(wait_random(15)))
