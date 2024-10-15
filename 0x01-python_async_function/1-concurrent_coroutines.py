3a446211c745dbb43e8fbd694c3ddbf718959736
chingling008 <130581550+chingling008@users.noreply.github.com>
2024-10-15 08:57:17 +0000

    task1Works

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Task 1's module."""
+
+import asyncio
+import random
+
+async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+    """Waits for n random delays and returns the results in ascending order."""
+
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    tasks = [wait_random(max_delay) for _ in range(n)]  # Create tasks
+    results = await asyncio.gather(*tasks)  # Gather results
+    return sorted(results)  # Sort results in ascending order
